71a2b94cdef4cf32f3fc0e9e6f6675b48ed22a1e
Mohammad shakil <43646753+Mohammad778@users.noreply.github.com>
2020-01-22 22:10:02 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace bankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var account = new BankAccount("Shakil",10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+
+        }
+       
+    }
+}
